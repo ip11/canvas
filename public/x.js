8dbf495cd6003775c7cc75e0d84ad9be9c6a99f8
Kamal Reddy <kamal.is.reddy@gmail.com>
2018-02-07 15:43:19 +0530

    xujgfky

--- public/x.js
+++ public/x.js
@@ -1,23 +1,45 @@
 
 
-//option A
-$("#form").submit(function(e){
-  e.preventDefault()
-  $.ajax({
-    method: "POST",
-    url: "/forajax",
-    data: {firstname: $("#firstname").val(), lastname: $("#lastname").val()}
-  })
-    .done(function (msg) {
-
-      console.log(msg);
 
 
-      $("#form").hide()
-      $('#users').append($('<p></p>').text(JSON.stringify(msg)));
 
-      // $("#users").append($("p"));
-    });
+$("#email,#password,#password2").on("input", function(){
+
+  var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  if( re.test($("#email").val()) && ($("#password").val()==$("#password2").val() ) && ($("#password").val().length!=0)){
+    $("#submitbutton").attr("disabled",false);
+  }else{
+    $("#submitbutton").attr("disabled",true);
+  }
 });
+
+
+
+
+
+
+
+
+
+//option A
+// $("#form").submit(function(e){
+//   e.preventDefault()
+//   $.ajax({
+//     method: "POST",
+//     url: "/forajax",
+//     data: {firstname: $("#firstname").val(), lastname: $("#lastname").val()}
+//   })
+//     .done(function (msg) {
+//
+//       console.log(msg);
+//
+//
+//       $("#form").hide()
+//       $('#users').append($('<p></p>').text(JSON.stringify(msg)));
+//
+//       // $("#users").append($("p"));
+//     });
+// });
 
 
